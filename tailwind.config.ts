6c1d7c17ff081ce23785d687d8ddbbf3cf87c553
Victor Chandra <victorchandra0911@gmail.com>
2024-11-21 17:56:45 +0700

    setup next js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,30 @@
 import type { Config } from "tailwindcss";
 
+const colors = {
+  color1: "#F97316",
+  color2: "#4722F5",
+  color3: "#0EA52F",
+  color4: "#08041A",
+  color5: "#FFBF32",
+
+  gray1: "#D1D0D5",
+  gray2: "#6C6D6F",
+  gray3: "#F4F5F7",
+  gray4: "#B9BBBE",
+};
+
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx,svg}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        ...colors,
+      },
+      fill: { ...colors },
+      fontFamily: {
+        sans: ["'Poppins', sans-serif"],
       },
     },
   },
